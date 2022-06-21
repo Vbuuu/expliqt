8de9e719723621720925641cf2e4c4148e184fc5
Vbuuu <m.rohm1@outlook.de>
2022-06-21 22:21:06 +0200

    hopefully fixed selection

--- versions.lua
+++ versions.lua
@@ -1,4 +1,4 @@
 return {
     [1] = { name = "Main", source = "https://raw.githubusercontent.com/Vbuuu/expliqt/main/", boot = "expliqt/boot.lua"},
-    [2] = { name = "Dev", source = "https://raw.githubusercontent.com/Vbuuu/expliqt/dev/", boot = "expliqt/boot.lua"},
+    [2] = { name = "Dev ", source = "https://raw.githubusercontent.com/Vbuuu/expliqt/dev/", boot = "expliqt/boot.lua"},
 }
