bdd81af72df6a67bd6745559c4a210b843236ba9
Vbuuu <m.rohm1@outlook.de>
2022-06-21 15:30:18 +0200

    Idoit forgot to lowercase

--- versions.lua
+++ versions.lua
@@ -1,4 +1,4 @@
 return {
-    [1] = { name = "Main", source = "https://raw.githubusercontent.com/Vbuuu/Expliqt/main/", boot = "Expliqt/boot.lua"},
-    [2] = { name = "Dev", source = "https://raw.githubusercontent.com/Vbuuu/Expliqt/dev/", boot = "Expliqt/boot.lua"},
+    [1] = { name = "Main", source = "https://raw.githubusercontent.com/Vbuuu/expliqt/main/", boot = "expliqt/boot.lua"},
+    [2] = { name = "Dev", source = "https://raw.githubusercontent.com/Vbuuu/expliqt/dev/", boot = "expliqt/boot.lua"},
 }
