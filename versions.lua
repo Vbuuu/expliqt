4cf512a2ad2dfc8d66984c706763908fad5f8b50
Vbuuu <m.rohm1@outlook.de>
2022-06-21 14:56:14 +0200

    Removed Version.lua

--- versions.lua
+++
@@ -1,4 +0,0 @@
-return {
-    [1] = { name = "Main", source = "https://raw.githubusercontent.com/Vbuuu/Expliqt/main/", boot = "Expliqt/boot.lua"},
-    [2] = { name = "Dev", source = "https://raw.githubusercontent.com/Vbuuu/Expliqt/dev/", boot = "Expliqt/boot.lua"},
-}
